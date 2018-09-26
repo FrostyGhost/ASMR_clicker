085c787d63e87092d9117489611f8af039a116e7
Andrew <ele33war@gmail.com>
2018-09-26 22:30:26 +0300

    First steps

---
+++ app/src/main/java/com/example/frostyghost/asmr_clicker/ItemsMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.frostyghost.asmr_clicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemsMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_menu);
+    }
+}
--- app/src/main/java/com/example/frostyghost/asmr_clicker/MainActivity.java
+++ app/src/main/java/com/example/frostyghost/asmr_clicker/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.frostyghost.asmr_clicker;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv_playground;
+    private TextView tv_clickmeter;
+    private int clickMeter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv_playground = findViewById(R.id.iv_playground);
+        tv_clickmeter = findViewById(R.id.tv_clickmeter);
+
+        clickMeter=0;
+        tv_clickmeter.setText(String.valueOf(clickMeter));
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.sponge_long_sound);
+
+        iv_playground.setClickable(true);
+        iv_playground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                clickMeter++;
+                tv_clickmeter.setText(String.valueOf(clickMeter));
+
+                //TODO
+            }
+        });
+    }
+
+    public void onMenu(View view){
+        startActivity(new Intent(this, ItemsMenuActivity.class));
     }
 }
